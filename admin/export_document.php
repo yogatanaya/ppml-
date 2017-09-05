9b0c5dd7cb5b1f0146f5744f7823f527dad3a03e
yogatanaya <tanayayoga@gmail.com>
2017-09-04 20:36:55 -1200

    Add files via upload

--- admin/export_document.php
+++ admin/export_document.php
@@ -11,13 +11,13 @@ header("content-disposition: attachment;filename=Report_Internal.xls");
 <table border='1'>
 <h3>
 <thead><tr>
-<td width=52>Jenis Dokumen</td>
 <td width=150>Judul Dokumen</td>
-<td width=180>Unit Terkait</td>
+<td width=100>Jenis Dokumen</td>
+<td width=180>Kode</td>
+<td width="180">Berlaku Efektif</td>
 <td width=150>Revisi Ke-</td>
-<td width=150>Status</td>
-<td width=150>Tanggal</td>
 <td width=150>Catatan</td>
+<td width=150>Status</td>
 </tr></thead>
 <h3><tbody>
 
@@ -32,6 +32,7 @@ if(isset($_POST['export'])){
     tb_jenis_dokumen.jenis_dokumen,
     tb_dokumen_baru.id_dokumen,
     tb_dokumen_baru.entry_date,
+    tb_dokumen_baru.kode,
     tb_dokumen_baru.nama_dokumen,
     tb_dokumen_baru.catatan,
     tb_dokumen_baru.keterangan,
@@ -45,21 +46,25 @@ if(isset($_POST['export'])){
     Inner Join status_dokumen ON status_dokumen.id_status_dokumen = tb_dokumen_baru.`status`
     Inner Join tb_jenis_dokumen ON tb_jenis_dokumen.id_jenis_dokumen = tb_dokumen_baru.jenis_dokumen
     Inner Join unit ON tb_admin.unit = unit.id_unit
-    where tb_dokumen_baru.entry_date Between '$dari' AND '$sampai'
+    where 
+        tb_dokumen_baru.entry_date Between '$dari' AND '$sampai'
     ");
 }
 while($data=mysql_fetch_array($query)){
 ?>
 
-<!--Menampilkan Data dari Tabel Seniman-->
+<!--
+Menampilkan Data dari Tabel Seniman-->
 <tr>
-    <td align="center"><?php echo $data['jenis_dokumen']; ?></td>
     <td align="center"><?php echo $data['nama_dokumen']; ?></td>
-    <td align="center"><?php echo $data['unit']; ?></td>
-    <td align="center"><?php echo $data['revisi']; ?></td>
-    <td align="center"><?php echo $data['status_dokumen']; ?></td>
+    <td align="center"><?php echo $data['jenis_dokumen']; ?></td>
+    <td align="center"><?php echo $data['kode']; ?></td>
     <td align="center"><?php echo $data['entry_date']; ?></td>
+    <td align="center"><?php echo $data['revisi']; ?></td>
     <td align="center"><?php echo $data['catatan']; ?></td>
+    <td align="center"><?php echo $data['status_dokumen']; ?></td>
+    
+
     </tr>
 
 <!--Penutup--> 
